176265c05f5b4c30a9eac403b2e6cf9fd19253d5
Matt Carter <m@ttcarter.com>
2018-03-09 11:41:59 +1000

    Huge refactor in now neatenSpec() works - more tests and configurable settings

--- src/shared.js
+++ src/shared.js
@@ -17,9 +17,11 @@ var _ = require('lodash');
 */
 $macgyver.forEach = function(spec, cb) {
 	var forEachScanner = (root, path) => {
-		var rootPath = (path ? path + '.' : '') + (root.id || '');
-		cb(root, rootPath);
-		if (_.isArray(root.items)) root.items.forEach(i => forEachScanner(i, rootPath));
+		if (_.isObject(root)) {
+			var rootPath = (path ? path + '.' : '') + (root && root.id ? root.id : '');
+			cb(root, rootPath);
+			if (_.isArray(root.items)) root.items.forEach(i => forEachScanner(i, rootPath));
+		}
 	};
 	forEachScanner(spec);
 },
@@ -45,34 +47,79 @@ $macgyver.flattenSpec = function(spec) {
 /**
 * Attempt to neaten up a 'rough' MacGyver spec into a pristine one
 * This function performs various sanity checks on nested elements e.g. checking each item has a valid ID and if not adding one
+* @param {boolean} [checkRootNoId=true] Root node should never have an ID
+* @param {boolean} [checkRootShowTitle=true] Verify that the root element has showTitle disabled
+* @param {boolean} [checkWidgetType=true] If the widget type is not registered change it to `mgText`
+* @param {boolean} [checkWidgetIds=true] Verify that each widget has an ID, if no ID is found - generate one
+* @param {boolean} [checkWidgetContainerIgnoreScope=true] Check that all mgContainers have ignoreScope enabled if its not specified
+* @param {function|null} [reporter] Reporter used when logging errors, defaults to using console.log
 */
-$macgyver.neatenSpec = function(spec) {
+$macgyver.neatenSpec = function(spec, options) {
+	var settings = _.defaults(options, {
+		checkRootNoId: true,
+		checkRootShowTitle: true,
+		checkWidgetType: true,
+		checkWidgetIds: true,
+		checkWidgetContainerIgnoreScope: true,
+		reporter: msg => console.log(msg),
+	});
+
+	if (!settings.reporter) settings.reporter = ()=> {}; // Make sure settings.reporter always exists, even if its a NoOp
+
 	// Force showTitle to be false on the root element if its not already set {{{
-	if (_.isUndefined(spec.showTitle)) spec.showTitle = false;
-	if (!spec.id) spec.id = 'ROOT'; // Force root element to have an ID
+	if (settings.checkRootNoId && spec.id) {
+		delete spec.id;
+		settings.reporter('Root node should not have an ID - removed');
+	}
+
+	if (settings.checkRootShowTitle && _.isUndefined(spec.showTitle)) {
+		spec.showTitle = false;
+		settings.reporter('Root node should not showTitle enabled - disabled');
+	}
 	// }}}
 
 	var flatSpec = $macgyver.flattenSpec(spec);
 	$macgyver.forEach(spec, (widget, path) => {
-		// Check that the widget is valid {{{
-		if (!$macgyver.widgets[widget.type]) {
-			console.log("Invalid or unregistered MacGyver widget '" + widget.type + "' - transforming into a mgText for now");
-			widget.type = 'mgText';
+		if (path == '') return; // Don't examine root elements
+
+		if (settings.checkWidgetType) {
+			// Check that the widget is valid {{{
+			if (!$macgyver.widgets[widget.type]) {
+				settings.reporter(`Invalid or unregistered MacGyver widget ${widget.type} - transforming into a mgText`);
+				widget.type = 'mgText';
+			}
 		}
 		// }}}
 
 		// Force all elements to have an ID {{{
-		if (!widget.id) {
-			// Make an ID based on the path {{{
-			var tryNumber = 0;
+		if (
+			settings.checkWidgetIds
+			&&
+			(
+				!widget.id
+				// BUGFIX: Rename all widgets beginning with $ROOT / ROOT to a randomly selected name {{{
+				// This patch is required to retroactively fix issues with the previous path-based fix, it can be removed after 2018-07-01 - MC
+				|| /^\$*ROOT/.test(widget.id)
+				// }}}
+			)
+		) {
+			// Make an ID based on the widget type + some junk {{{
+			var oldId = widget.id;
 			var tryId;
 			do {
-				tryId = '$' + path + '$' + tryNumber++;
+				tryId = widget.type + '-' + _.times(5, i => _.sample('abcdefghijklmnopqrstuvwxyz').split('')).join('');
 			} while (flatSpec[tryId]);
 			widget.id = tryId;
+			settings.reporter(
+				(oldId ? `Invalid widget ID ${oldId}` : `Non-existant ID for widget ${widget.type}`)
+				+ `generated new ID as ${widget.id}`
+			);
 			// }}}
 
-			if (widget.type == 'mgContainer') widget.ignoreScope = true; // Containers - enable ignoreScope also
+			if (settings.checkWidgetContainerIgnoreScope && widget.type == 'mgContainer' && _.isUndefined(widget.ignoreScope)) {
+				widget.ignoreScope = true; // Containers - enable ignoreScope also
+				settings.reporter('Forced mgContainer to ignoreScope');
+			}
 		}
 		// }}}
 	});
